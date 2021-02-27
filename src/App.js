2421c253a9e1816dc912ff0dfbf3f1e41c16c006
Agunovvski <33866004+Agunovvski@users.noreply.github.com>
2021-02-27 23:40:48 +0100

    slug sinlge notfound setup

--- src/App.js
+++ src/App.js
@@ -5,32 +5,32 @@ import './assets/templateStyles.css'
 import Nav from './components/nav/Nav'
 import Footer from './components/footer/Footer'
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import Home from './pages/home/Home'
-import Work from './pages/work/Work'
+// import Work from './pages/work/Work'
 import About from './pages/about/About'
 import Post from './pages/work/projects/Post'
 import SinglePost from './pages/work/projects/SinglePost'
 import Project from './pages/work/projects/Project'
+import NotFound from './pages/NotFound';
 
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route path='/' exact component={Home}></Route>
-          <Route path='/my-work' component={Project}></Route>
-          <Route path='/about' component={About}></Route>
-          <Route path='/post' component={Post}></Route>
-          <Route path='/post/:slug' component={SinglePost}></Route>
-          {/* <Route path='/project' component={Project}></Route> */}
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route path='/' exact component={Home}></Route>
+        <Route path='/my-work' component={Project}></Route>
+        <Route path='/about' component={About}></Route>
+        <Route path='/post/:slug' component={SinglePost}></Route>
+        <Route path='/post' component={Post}></Route>
+        <Route component={NotFound}></Route>
+        {/* <Route path='/project' component={Project}></Route> */}
+      </Switch>
+      <Footer />
+    </div>
   );
 }
 
--- src/pages/work/projects/SinglePost.js
+++ src/pages/work/projects/SinglePost.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import sanityClient from '../../../client.js'
+import imageUrlBuilder from '@sanity/image-url'
+import BlockContent from '@sanity/block-content-to-react'
+
+import { Skeleton } from 'antd'
+
+const builder = imageUrlBuilder(sanityClient)
+
+function urlFor(source) {
+    return builder.image(source)
+}
 
 export default function SinglePost() {
+
+    const [singlePost, setSinglePost] = useState(null)
+    const { slug } = useParams()
+
+    useEffect(() => {
+        sanityClient.fetch(`*[slug.current == '${slug}']{
+            title,
+            _id,
+            slug,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                }
+            },
+            body,
+            'name': author->name,
+            'authorImage': author->image
+        }`)
+            .then((data) => setSinglePost(data[0]))
+            .catch(console.error)
+    }, [slug])
+
+
+    if (!singlePost)
+        return <Skeleton active='true' />
+
     return (
-        <div>
-            <h1>Single Post Component</h1>
-        </div>
+        <section>
+            <div className='template-width'>
+                <header>
+                    <div>
+                        <div>
+                            <h1>{singlePost.title}</h1>
+                            <div>
+                                <img src={urlFor(singlePost.authorImage.url())} alt={singlePost.name} />
+                                <p>{singlePost.name}</p>
+                            </div>
+                        </div>
+                        <img src={singlePost.mainImage.asset.url} alt={singlePost.title} />
+                    </div>
+                </header>
+                <div><BlockContent blocks={singlePost.body} projectId='ispob9gx' dataset='production'></BlockContent></div>
+            </div>
+        </section>
     )
 }
