4004afb6016a9830346f7574db44c555a144bc7b
Agunovvski <33866004+Agunovvski@users.noreply.github.com>
2021-02-27 12:40:32 +0100

    project setup

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import Home from '../../pages/home/Home'
 import Work from '../../pages/work/Work'
+import About from '../../pages/about/About'
 
 export default function Nav() {
     return (
         <Router>
             <nav>
                 <Link to='/'>Home</Link>
-                <Link to='my-work'>My work</Link>
+                <Link to='/my-work'>My work</Link>
+                <Link to='/about'>About me</Link>
             </nav>
             <Switch>
                 <Route path='/' exact component={Home}></Route>
-                <Route path='my-work' component={Work}></Route>
+                <Route path='/my-work' component={Work}></Route>
+                <Route path='/about' component={About}></Route>
             </Switch>
         </Router>
     )
