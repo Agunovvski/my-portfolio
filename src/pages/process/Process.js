f778541068f9373a7564bec1c4d8e10a518edc8d
Agunovvski <33866004+Agunovvski@users.noreply.github.com>
2022-02-07 21:53:08 +0100

    process list and cards setup

--- src/pages/process/Process.js
+++ src/pages/process/Process.js
@@ -1,4 +1,4 @@
-import { Breadcrumb, Card, Timeline } from 'antd';
+import { Breadcrumb, Card, List, Tag, Timeline } from 'antd';
 import BreadcrumbItem from 'antd/lib/breadcrumb/BreadcrumbItem';
 import TimelineItem from 'antd/lib/timeline/TimelineItem';
 import React, { useEffect } from 'react';
@@ -14,6 +14,41 @@ export default function Process() {
         window.scrollTo(0, 0)
     }, [])
 
+
+    // lists methodologies render
+
+    const understandList = [
+        'Interviews',
+        'Customer Journey',
+        'Participant observation',
+        'Card sorting',
+        'Online analytics',
+    ]
+
+    const defineList = [
+        'Interviews',
+        'Customer Journey',
+        'Participant observation',
+        'Card sorting',
+        'Online analytics',
+    ]
+
+    const ideateList = [
+        'Interviews',
+        'Customer Journey',
+        'Participant observation',
+        'Card sorting',
+        'Online analytics',
+    ]
+
+    const iterateList = [
+        'Interviews',
+        'Customer Journey',
+        'Participant observation',
+        'Card sorting',
+        'Online analytics',
+    ]
+
     return <section>
         <div className='template-width'>
             <Breadcrumb>
@@ -22,7 +57,7 @@ export default function Process() {
                 </BreadcrumbItem>
             </Breadcrumb>
             <h1>Design process.</h1>
-            <p>Essential steps to deliver meaningful experiences:</p>
+            <p>To me, the design thinking philosophy is something I live by. To deliver meaningful experiences for the end user I follow these main steps:</p>
             <Timeline mode='left' pending='Iterate'>
                 <TimelineItem>
                     Understand
@@ -34,13 +69,53 @@ export default function Process() {
                     Ideate
                 </TimelineItem>
             </Timeline>
-            <Card
-                bordered={false}
-            >
-                <h3>Understand</h3>
-                <p>Testing description</p>
-            </Card>
+            <div className='process-container'>
+                <Card
+                    bordered={false}
+                >
+                    <h3>Understand</h3>
+                    <p>This is the first thing to consider when starting out any project: understanding the users, their needs, and problems.</p>
+                    <h5>Favorite metholodogies:</h5>
+                    <List itemLayout='horizontal' dataSource={understandList} renderItem={item => (
+                        <Tag>{item}</Tag>
+                    )}>
+                    </List>
+                </Card>
+                <Card
+                    bordered={false}
+                >
+                    <h3>Define</h3>
+                    <p>This is the first thing to consider when starting out any project: understanding the users, their needs, and problems.</p>
+                    <h5>Favorite metholodogies:</h5>
+                    <List itemLayout='horizontal' dataSource={defineList} renderItem={item => (
+                        <Tag>{item}</Tag>
+                    )}>
+                    </List>
+                </Card>
+                <Card
+                    bordered={false}
+                >
+                    <h3>Ideate</h3>
+                    <p>This is the first thing to consider when starting out any project: understanding the users, their needs, and problems.</p>
+                    <h5>Favorite metholodogies:</h5>
+                    <List itemLayout='horizontal' dataSource={ideateList} renderItem={item => (
+                        <Tag>{item}</Tag>
+                    )}>
+                    </List>
+                </Card>
+                <Card
+                    bordered={false}
+                >
+                    <h3>Iterate</h3>
+                    <p>This is the first thing to consider when starting out any project: understanding the users, their needs, and problems.</p>
+                    <h5>Favorite metholodogies:</h5>
+                    <List itemLayout='horizontal' dataSource={iterateList} renderItem={item => (
+                        <Tag>{item}</Tag>
+                    )}>
+                    </List>
+                </Card>
+            </div>
         </div>
 
-    </section>;
+    </section >;
 }
