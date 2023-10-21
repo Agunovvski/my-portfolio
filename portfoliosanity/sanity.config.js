e8b70978973291593b159fedbe453de453c9350b
Agunovvski <33866004+Agunovvski@users.noreply.github.com>
2023-10-21 23:16:08 +0200

    install uninstalls and sanity configs added

---
+++ portfoliosanity/sanity.config.js
@@ -0,0 +1,12 @@
+// sanity.config.js
+import { defineConfig } from "sanity";
+
+export default defineConfig({
+    title: "portfolio-sanity",
+    projectId: "ispob9gx",
+    dataset: "production",
+    plugins: [],
+    schema: {
+        types: [],
+    },
+});
